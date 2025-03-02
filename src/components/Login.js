a8358a59514fb5de70c173c134b4198ff43a555c
Santiago <j.santiagolseon@gmail.com>
2025-03-02 06:39:15 -0500

    feat: :sparkles: Primer componente de prueba
    Se crea un componente llamado Login que se espera sea implementado durante el avance del proyecto

---
+++ src/components/Login.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export const Login = () => {
+  return (
+
+    // div general para la implementacion de ingreso del usuario
+    <div className='credentials'>
+        <h1>Bienvenido</h1>
+        <h5>Por favor Ingrese sus credenciales a continuacion:</h5>
+
+        
+
+    </div>
+  )
+}
